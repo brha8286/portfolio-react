73ad4c087b286087bc581bbb334583a1b72730d4
brandon <brha8286@colorado.edu>
2020-05-31 01:06:45 -0600

    Add partly-opaque white background behind text over banner

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -6,11 +6,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
 import ProjectCard from './ProjectCard';
-import {PROJECTS} from '../projects';
+import { PROJECTS } from '../projects';
 import bannerImage from "../resources/banner.jpg";
 
 const useStyles = makeStyles((theme) => ({
-  
+
   heroContent: {
     backgroundColor: theme.palette.background.paper,
     backgroundImage: `url(${bannerImage})`,
@@ -19,6 +19,10 @@ const useStyles = makeStyles((theme) => ({
     backgroundAttachment: 'fixed',
     padding: theme.spacing(8, 0, 6),
     minHeight: '100vh',
+  },
+  heroOverlay: {
+    backgroundColor: 'rgba(255, 255, 255, 0.6)',
+    padding: '20px 0px',
   },
   heroButtons: {
     marginTop: theme.spacing(4),
@@ -52,30 +56,32 @@ export default function Homepage() {
       <main>
         {/* Hero unit */}
         <div className={classes.heroContent}>
-          <Container maxWidth="sm">
-            <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Brandon Harris
+          <div className={classes.heroOverlay}>
+            <Container maxWidth="sm">
+              <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
+                Brandon Harris
             </Typography>
-            <Typography variant="h5" align="center" color="textPrimary" paragraph>
-              Something short and leading about the collection below—its contents, the creator, etc.
-              Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-              entirely.
+              <Typography variant="h5" align="center" color="textPrimary" paragraph>
+                Something short and leading about the collection below—its contents, the creator, etc.
+                Make it short and sweet, but not too short so folks don&apos;t simply skip over it
+                entirely.
             </Typography>
-            <div className={classes.heroButtons}>
-              <Grid container spacing={2} justify="center">
-                <Grid item>
-                  <Button variant="contained" color="primary">
-                    Main call to action
+              <div className={classes.heroButtons}>
+                <Grid container spacing={2} justify="center">
+                  <Grid item>
+                    <Button variant="contained" color="primary">
+                      Main call to action
                   </Button>
-                </Grid>
-                <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Secondary action
+                  </Grid>
+                  <Grid item>
+                    <Button variant="outlined" color="primary">
+                      Secondary action
                   </Button>
+                  </Grid>
                 </Grid>
-              </Grid>
-            </div>
-          </Container>
+              </div>
+            </Container>
+          </div>
         </div>
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
@@ -85,11 +91,12 @@ export default function Homepage() {
           <Grid container spacing={6}>
             {PROJECTS.map((card) => (
               <Grid item key={card.id} xs={12} sm={6} md={4}>
-                <ProjectCard {...card}/>
+                <ProjectCard {...card} />
               </Grid>
             ))}
           </Grid>
         </Container>
+        <div />
       </main>
     </React.Fragment>
   );
