bf8df08d49d3c26fc686aeb75ce1645736bba03c
brandon <brha8286@colorado.edu>
2020-08-10 21:34:29 -0700

    Updates

---
+++ precache-manifest.20bf2aab453d1d166872778a343107b6.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6d511c7cd5a7a69d8fdfde44cb42e10",
+    "url": "/portfolio-react/index.html"
+  },
+  {
+    "revision": "323d0c042e6a8dd983fa",
+    "url": "/portfolio-react/static/css/2.40a775a0.chunk.css"
+  },
+  {
+    "revision": "a9aa5f37c3d5f93b4b62",
+    "url": "/portfolio-react/static/css/main.68bee0f2.chunk.css"
+  },
+  {
+    "revision": "323d0c042e6a8dd983fa",
+    "url": "/portfolio-react/static/js/2.27bf1488.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio-react/static/js/2.27bf1488.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a9aa5f37c3d5f93b4b62",
+    "url": "/portfolio-react/static/js/main.b8613f29.chunk.js"
+  },
+  {
+    "revision": "2f26d0aed242c262185b",
+    "url": "/portfolio-react/static/js/runtime-main.a4de92d3.js"
+  },
+  {
+    "revision": "cc307757efd434dad5b3313417c45c29",
+    "url": "/portfolio-react/static/media/assembled.cc307757.jpg"
+  },
+  {
+    "revision": "c76ae3d8f5fec1c877e35306edbc236d",
+    "url": "/portfolio-react/static/media/banner.c76ae3d8.jpg"
+  },
+  {
+    "revision": "925ad7ab0da00016851b9f36a3bee283",
+    "url": "/portfolio-react/static/media/buckling_annotated.925ad7ab.png"
+  },
+  {
+    "revision": "7f294834c21b5541fc1ab7cb148dd3fe",
+    "url": "/portfolio-react/static/media/buckling_rig.7f294834.png"
+  },
+  {
+    "revision": "86b948d0263f7a0648415ceac4b05996",
+    "url": "/portfolio-react/static/media/components.86b948d0.png"
+  },
+  {
+    "revision": "3f56155deeb106e84c72d2e472aeeadc",
+    "url": "/portfolio-react/static/media/coupling.3f56155d.PNG"
+  },
+  {
+    "revision": "5a772ed83e88c52593bea97fb13f07fc",
+    "url": "/portfolio-react/static/media/hotfire.5a772ed8.jpg"
+  },
+  {
+    "revision": "c8c0bb1a5c7bfe16d8f0dcb0e5f9b1be",
+    "url": "/portfolio-react/static/media/nozzle.c8c0bb1a.png"
+  },
+  {
+    "revision": "2e9ba33c19860415ef94fc9717f542bf",
+    "url": "/portfolio-react/static/media/pitch.2e9ba33c.png"
+  },
+  {
+    "revision": "4b55521f85d82de21c0baac88b1d6558",
+    "url": "/portfolio-react/static/media/roll.4b55521f.png"
+  },
+  {
+    "revision": "08261bc864c4cabf6a1b4820abc64313",
+    "url": "/portfolio-react/static/media/system.08261bc8.png"
+  },
+  {
+    "revision": "8bd7ffe8b3de7cc4bb48e949c6b8ed9d",
+    "url": "/portfolio-react/static/media/system.8bd7ffe8.png"
+  },
+  {
+    "revision": "bd0e5e26a835bcd51e385174421034e4",
+    "url": "/portfolio-react/static/media/yaw.bd0e5e26.png"
+  }
+]);
