6913257441c0a1343aaf7b634f18efd5b06dbfe9
brandon <brha8286@colorado.edu>
2020-09-14 20:56:00 -0700

    Updates

---
+++ precache-manifest.79208d1bf6cbedfbc53fbc422348e902.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1701baf27f02df8fa82065ef9c0e4bd7",
+    "url": "http://brandonharris.tech/index.html"
+  },
+  {
+    "revision": "f606990189b75a540e60",
+    "url": "http://brandonharris.tech/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "66690783f6ef56d82a5f",
+    "url": "http://brandonharris.tech/static/css/main.99abb98f.chunk.css"
+  },
+  {
+    "revision": "f606990189b75a540e60",
+    "url": "http://brandonharris.tech/static/js/2.e36423c6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "http://brandonharris.tech/static/js/2.e36423c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66690783f6ef56d82a5f",
+    "url": "http://brandonharris.tech/static/js/main.b63151dc.chunk.js"
+  },
+  {
+    "revision": "657352670f088606fff6",
+    "url": "http://brandonharris.tech/static/js/runtime-main.19b67b37.js"
+  },
+  {
+    "revision": "a3b7b2d5c33174107f956cac60f35a71",
+    "url": "http://brandonharris.tech/static/media/BrandonHarris.a3b7b2d5.pdf"
+  },
+  {
+    "revision": "0474e6389466667cd24d0d9018fa5fab",
+    "url": "http://brandonharris.tech/static/media/about.0474e638.png"
+  },
+  {
+    "revision": "19c33ae4251bc72b60d7cc9c35d917b0",
+    "url": "http://brandonharris.tech/static/media/assembled.19c33ae4.png"
+  },
+  {
+    "revision": "c76ae3d8f5fec1c877e35306edbc236d",
+    "url": "http://brandonharris.tech/static/media/banner.c76ae3d8.jpg"
+  },
+  {
+    "revision": "4a6fe98cb6ac2660882d49cf77aab37d",
+    "url": "http://brandonharris.tech/static/media/bottom_0.4a6fe98c.png"
+  },
+  {
+    "revision": "6a1f6c4189b2db7c3bd039785dc3d8b6",
+    "url": "http://brandonharris.tech/static/media/bottom_1.6a1f6c41.png"
+  },
+  {
+    "revision": "d29ea7691b23aa459d7c74291e369774",
+    "url": "http://brandonharris.tech/static/media/buckling_annotated.d29ea769.png"
+  },
+  {
+    "revision": "d7bc0c2c051ad232a64c57b143a159cc",
+    "url": "http://brandonharris.tech/static/media/buckling_rig.d7bc0c2c.png"
+  },
+  {
+    "revision": "0274ce6388406729b5ceeb09f1a64838",
+    "url": "http://brandonharris.tech/static/media/components.0274ce63.png"
+  },
+  {
+    "revision": "44545185b729347d49b66e6e5721ee23",
+    "url": "http://brandonharris.tech/static/media/coupling.44545185.PNG"
+  },
+  {
+    "revision": "123e61327d4e19a89e63aa4b70f5fce3",
+    "url": "http://brandonharris.tech/static/media/hotfire.123e6132.png"
+  },
+  {
+    "revision": "865e080e9119b755e386f66c1f12dc8a",
+    "url": "http://brandonharris.tech/static/media/nozzle.865e080e.png"
+  },
+  {
+    "revision": "7958b03478c1a40f0ea0d27c81a48174",
+    "url": "http://brandonharris.tech/static/media/pitch.7958b034.png"
+  },
+  {
+    "revision": "5207e9295f26397e32c76f8b504e57a4",
+    "url": "http://brandonharris.tech/static/media/riser_assembled.5207e929.png"
+  },
+  {
+    "revision": "7c248d8b18af5f7d1ab05a1b2c263251",
+    "url": "http://brandonharris.tech/static/media/riser_parts.7c248d8b.png"
+  },
+  {
+    "revision": "b5624c60c720b221ea7631f203e79eda",
+    "url": "http://brandonharris.tech/static/media/roll.b5624c60.png"
+  },
+  {
+    "revision": "c35a8691b63319b658dce9a36beb10df",
+    "url": "http://brandonharris.tech/static/media/snap_fit.c35a8691.png"
+  },
+  {
+    "revision": "81f4eaf46ecf5246be433e984216f914",
+    "url": "http://brandonharris.tech/static/media/system.81f4eaf4.png"
+  },
+  {
+    "revision": "dac410f59b95d10e808ac19101c597cc",
+    "url": "http://brandonharris.tech/static/media/system.dac410f5.png"
+  },
+  {
+    "revision": "67a9c68689edbfecba3de7519b5fcf53",
+    "url": "http://brandonharris.tech/static/media/top_glow.67a9c686.png"
+  },
+  {
+    "revision": "866ede2e81e042b0a2edeb7319ec05aa",
+    "url": "http://brandonharris.tech/static/media/yaw.866ede2e.png"
+  }
+]);
