611ed5e10f92e686cc0e61bcb186cde4b2b26a1f
brandon <brha8286@colorado.edu>
2020-09-16 01:16:44 -0700

    Adding social icons

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,13 +3,16 @@ import React from "react";
 import classes from './footer.module.scss';
 import {Container} from "react-bootstrap";
 import clsx from "clsx";
+import { SocialIcon } from "react-social-icons";
 
 export const Footer = ({ style, className }) => {
   return (
     <footer className={clsx(classes.footer, className)} style={style}>
-      <Container>
+      <SocialIcon url="mailto:brha8286@gmail.com" target="_blank" rel="noopener noreferrer" className={classes.icons}/>
+      <SocialIcon url="https://www.linkedin.com/in/brandon-harris-co/" target="_blank" rel="noopener noreferrer" className={classes.icons}/>
+      {/* <Container> */}
         {'Copyright © '} Brandon Harris {new Date().getFullYear()}{'.'}
-      </Container>
+      {/* </Container> */}
     </footer>
   )
 };
